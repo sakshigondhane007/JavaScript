c7ef6cecf31ae367b6c753f72064ec01450f7177
sakshigondhane007 <sakshigondhane007@gmail.com>
2026-04-18 06:47:20 +0000

    learn var let and const

---
+++ basics/02_var_let_const.js
@@ -0,0 +1,16 @@
+console.log("JavaScript tutorial 3: var, let and const")
+//var a = 45;
+//var a = 6;
+
+let b = "Sakshi";
+const author = "sakshi"
+//author = 5 // throws an error because constant cannot be changed
+b = 4
+const sakshi = 0;
+let c = null
+let d = undefined
+{
+    let b = 'this'
+    console.log(b)
+}
+console.log(b)
