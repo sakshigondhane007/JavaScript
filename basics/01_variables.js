5556833ccd8f61c2fb7d70b8c2508b2cb6c00c2e
sakshigondhane007 <sakshigondhane007@gmail.com>
2026-04-18 06:38:37 +0000

    learn variables

--- basics/01_variables.js
+++ basics/01_variables.js
@@ -1,3 +1,12 @@
 let a = 4
 let b = 5
 console.log(a);
+
+console.log("i am sakshi")
+var c = 67 // a contains 67
+console.log(c)
+
+c = "sakshi"
+console.log(c)
+//let 3sakshi = 7 //not allowed this will throw an error
+//let var = 7
